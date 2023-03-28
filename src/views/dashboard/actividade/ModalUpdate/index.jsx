3760bd75e4a9e69c7505826850650e5b82d5f108
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-03-29 00:29:49 +0100

    feat: finalizando o crud de actividade

--- src/views/dashboard/actividade/ModalUpdate/index.jsx
+++ src/views/dashboard/actividade/ModalUpdate/index.jsx
@@ -10,8 +10,16 @@ function ModalUpdate({ onClose, item, mutate, setShowModalUpadate }) {
       nome: item?.nome,
       fotoUrl: item?.fotoUrl,
       descricao: item?.descricao,
+      data: item?.data,
+      organizador: item?.organizador
     },
-    validationSchema: yup.object({}),
+    validationSchema: yup.object({
+      nome: yup.string().required("Este campo é obrigatório"),
+      fotoUrl: yup.string().required("Este campo é obrigatório"),
+      descricao: yup.string().required("Este campo é obrigatório"),
+      data: yup.date().min(new Date(), 'A data deve ser maior ou igual ao dia de hoje').required("Este campo é obrigatório"),
+      organizador: yup.string().required("Este campo é obrigatório"),      
+    }),
     onSubmit: async (data) =>{
       if(item?.fotoUrl === data?.fotoUrl){
         try{
@@ -65,6 +73,7 @@ function ModalUpdate({ onClose, item, mutate, setShowModalUpadate }) {
           <Form onSubmit={formik?.handleSubmit} encType="multipart/form-data">
             <Container>
               <Col xs={12} sm={12} md={12} lg={50} xl={20} className="mb-3 ">
+                <div>
                 <Form.Label htmlFor="validationCustom05">
                   Nome
                 </Form.Label>
@@ -75,10 +84,58 @@ function ModalUpdate({ onClose, item, mutate, setShowModalUpadate }) {
                   type="text"
                   id="nome"
                 />
-                <Form.Group className="mb-3 form-group mt-2">
+                {formik?.touched?.nome && formik?.errors?.nome ? (
+                  <label className="mt-1 text-danger">  
+                    {formik?.errors?.nome}
+                  </label>
+                ): null}
+                </div>
+                <div>
+                <Form.Label htmlFor="validationCustom05">
+                  Organizador
+                </Form.Label>
+                <Form.Control
+                  onChange={formik.handleChange}
+                  name="organizador"
+                  value={formik.values.organizador}
+                  type="text"
+                  id="organizador"
+                />
+                {formik?.touched?.organizador && formik?.errors?.organizador ? (
+                  <label className="mt-1 text-danger">  
+                    {formik?.errors?.organizador}
+                  </label>
+                ): null}
+                </div>
+                <div>
+                <Form.Label htmlFor="validationCustom05">
+                  Data
+                </Form.Label>
+                <Form.Control
+                  onChange={formik.handleChange}
+                  name="data"
+                  value={formik.values.data}
+                  type="date"
+                  id="data"
+                />
+                {formik?.touched?.data && formik?.errors?.data ? (
+                  <label className="mt-1 text-danger">  
+                    {formik?.errors?.data}
+                  </label>
+                ): null}
+                </div>
+                <div>
+                <Form.Group className="mb-3 form-group mt-1">
                                 <Form.Label htmlFor="exampleFormControlTextarea1">Descrição</Form.Label>
                                 <Form.Control as="textarea" id="descricao" value={formik.values.descricao} name="descricao" onChange={formik.handleChange} rows="5" />
                 </Form.Group>
+                {formik?.touched?.descricao && formik?.errors?.descricao ? (
+                  <label className="text-danger">  
+                    {formik?.errors?.descricao}
+                  </label>
+                ): null}
+                </div>
+                <div>
                 <Form.Group className="mb-3 form-group mt-2">
                   <Form.Label className="custom-file-input">Escolha o ficheiro</Form.Label>
                   <Form.Control
@@ -90,6 +147,12 @@ function ModalUpdate({ onClose, item, mutate, setShowModalUpadate }) {
                       }}  
                   />
               </Form.Group>
+              {formik?.touched?.fotoUrl && formik?.errors?.fotoUrl ? (
+                  <label className="mt-1 text-danger">  
+                    {formik?.errors?.fotoUrl}
+                  </label>
+                ): null}
+              </div>
               </Col>
             </Container>
       <Modal.Footer>
