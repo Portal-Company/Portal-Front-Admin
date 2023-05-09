062b55aee07a5f0def0f3ed45833a065b5ef74a4
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-05-09 08:07:51 +0100

    feat: adicionando validacao de imagen na area de curso

--- src/views/dashboard/curso/curso-add.jsx
+++ src/views/dashboard/curso/curso-add.jsx
@@ -29,7 +29,21 @@ const Cursos = () => {
     },
     validationSchema: yup.object({
       nome: yup.string().required("Este campo é obrigatório"),
-      fotoUrl: yup.string().required("Este campo  é obrigatório"),
+      fotoUrl: yup
+        .mixed()
+        .test(
+          "isImage",
+          "Por favor selecione um arquivo de imagem válido!",
+          (value) => {
+            if (!value) return true; // permite que o campo seja vazio
+            return (
+              value &&
+              ["image/png", "image/jpg", "image/jpeg", "image/gif"].includes(
+                value.type
+              )
+            );
+          }
+        ),
       descricao: yup.string().required("Este campo é obrigatório"),
       areaDeFormacaoId: yup.string().required("Este campo é obrigatório"),
     }),
